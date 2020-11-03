95ac90ae576302bf642a8cb3ca72e2e47fb08124
hadrihl <hadrihl@outlook.com>
2020-11-03 11:14:02 +0800

    Initial commit.

---
+++ chap4/UDPServer.java
@@ -0,0 +1,31 @@
+import java.net.*;
+import java.io.*;
+
+public class UDPServer {
+
+	public static void main(String[] args) {
+		DatagramSocket aSocket = null;
+		
+		try {
+			aSocket = new DatagramSocket(55555);
+			byte[] buffer = new byte[1000];
+			
+			while(true) {
+				// Create a datagram to receive data
+				DatagramPacket request = new DatagramPacket(buffer, buffer.length);
+				aSocket.receive(request);
+				
+				// @debug
+				System.out.println("Client: " + request);
+				
+				DatagramPacket reply = new DatagramPacket(request.getData(), request.getLength(), request.getAddress(), request.getPort());
+				aSocket.send(reply);
+				
+			}
+		} catch (SocketException e) { System.out.println("Socket: " + e.getMessage()); }
+		catch (IOException e) { System.out.println("IO: " + e.getMessage()); }
+		finally { if(aSocket != null) aSocket.close(); }
+
+	}
+
+}
